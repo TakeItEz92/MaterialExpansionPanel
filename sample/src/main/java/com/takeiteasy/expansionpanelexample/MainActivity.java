2922e66b63d0a8aba098097d4a123bf879b05e04
TakeItEz <vip.takeiteasy@gmail.com>
2016-10-19 15:55:06 +0300

    Added first poor variant of implementation.

--- sample/src/main/java/com/takeiteasy/expansionpanelexample/MainActivity.java
+++ sample/src/main/java/com/takeiteasy/expansionpanelexample/MainActivity.java
@@ -1,7 +1,7 @@
-package com.takeiteasy.materialexpansionpanel;
+package com.takeiteasy.expansionpanelexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
